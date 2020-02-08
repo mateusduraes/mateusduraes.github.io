eca8884a170e5e705c5217953a7156aa60902406
Mateus Duraes <mduraes1994@gmail.com>
2020-02-08 02:28:35 -0300

    Blog list and blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,38 @@ module.exports = {
     {
       resolve: "gatsby-transformer-remark",
       options: {
-        plugins: [], // just in case those previously mentioned remark plugins sound cool :)
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: "superscript",
+                  extend: "javascript",
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              prompt: {
+                user: "root",
+                host: "localhost",
+                global: false,
+              },
+              escapeEntities: {},
+            },
+          },
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/templates/article.js
+++ src/templates/article.js
@@ -1,4 +1,55 @@
 import React from "react"
-const Article = () => <p>Teste</p>
+import Layout from "../components/Layout"
+import styled from "styled-components"
+
+const ArticleTitle = styled.h1`
+  font-size: 2rem;
+  font-weight: 400;
+  margin-bottom: 0.8rem;
+  text-align: center;
+`
+
+const ArticleDate = styled.time`
+  font-weight: 300;
+  color: #a0a0a0;
+  font-size: 12px;
+  text-transform: uppercase;
+  display: block;
+  text-align: center;
+`
+
+const ArticleWrapper = styled.div`
+  padding: 5rem 5rem 0;
+`
+
+const Article = props => {
+  const article = props.data.markdownRemark
+
+  return (
+    <Layout>
+      <ArticleTitle>{article.frontmatter.title}</ArticleTitle>
+      <ArticleDate>{article.frontmatter.date}</ArticleDate>
+      <ArticleWrapper>
+        <div dangerouslySetInnerHTML={{ __html: article.html }} />
+      </ArticleWrapper>
+    </Layout>
+  )
+}
 
 export default Article
+
+export const query = graphql`
+  query Article($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      fields {
+        slug
+      }
+      frontmatter {
+        date(locale: "en", formatString: "YYYY[,] MMMM DD")
+        description
+        title
+      }
+    }
+  }
+`
